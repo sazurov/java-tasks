46dc52ca5c020acf08b581c7aaaa85a66afa6ae3
sazurov <sazuroov@gmail.com>
2024-01-27 19:55:35 +0300

    commit

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -2,40 +2,60 @@ package bank;
 
 import java.util.Scanner;
 
+class Customer  {
+    private String name;
+    private String docs;
+
+    public Customer (String name , String docs) {
+        this.name = name;
+        this.docs = docs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDocs() {
+        return docs;
+    }
+}
+
 class Solution {
     private Scanner scan = new Scanner(System.in);
     private int customersCount = 0;
-    private String[] identifyDocs = new String[customersCount];
+    private Customer[] customers;
 
     private String getData(String text) {
         System.out.println(text);
         return scan.nextLine();
     }
 
-    private boolean validate (String[] arr) {
+    private boolean validate(Customer[] arr) {
         return arr.length == customersCount;
     }
 
-    public void fillArr () {
+    public void fillArr() {
         customersCount = Integer.parseInt(getData("Введите количество участников в сделке"));
-        identifyDocs = new String[customersCount];
-        System.out.println("Внести документы, удостоверяющие личность участника: ");
+        customers = new Customer[customersCount];
+        System.out.println("Внесите документы, удостоверяющие личность участника: ");
 
-        for (int i = 0 ; i < customersCount ; i++) {
-            identifyDocs[i] = String.valueOf(getData(""));
+        for (int i = 0; i < customersCount; i++) {
+            String name = getData("Имя:");
+            String docs = getData("Документ, удостоверяющий личность:");
+            customers[i] = new Customer(name, docs);
         }
 
-        if (validate(identifyDocs)) {
+        if (validate(customers)) {
             System.out.println("Данные успешно занесены:");
-            for (String doc : identifyDocs) {
-                System.out.println(doc);
+            for (Customer doc : customers) {
+                System.out.println(doc.getName() + ": " + doc.getDocs());
             }
         } else {
             System.out.println("Ошибка при вводе данных участников сделки!");
         }
     }
-
 }
+
 
 public class Bank {
     public static void main (String[] args) {
